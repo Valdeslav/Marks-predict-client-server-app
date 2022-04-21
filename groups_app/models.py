62bd4c68169e5464caa294ee21eaee8454125b5d
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2022-04-22 00:25:07 +0300

    added group, faculty, and speciality models

--- groups_app/models.py
+++ groups_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Speciality(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Group(models.Model):
+    number = models.DecimalField(decimal_places=2, max_digits=0)
+    speciality = models.ForeignKey(Speciality, on_delete=models.PROTECT)
+    faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT)
