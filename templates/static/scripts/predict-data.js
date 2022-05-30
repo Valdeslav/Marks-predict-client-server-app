38aba52e5d5e3a390dc9705efa57c5c21434593e
Vladislav Tsarenko <vadiktsarenko@mail.ru>
2022-05-30 22:06:10 +0300

    added loginrequired to views. Added selectAll checkboxes to prediction form

--- templates/static/scripts/predict-data.js
+++ templates/static/scripts/predict-data.js
@@ -1,3 +1,66 @@
+window.onload = function (){
+    setAllStudentCheckbox();
+    setAllSubjectCheckbox();
+
+    let studentCheckboxes = document.getElementsByName('student_id');
+    let studentsAllCheckbox = document.getElementById('check_all_students');
+    let subjectCheckboxes = document.getElementsByName('subject_id');
+    let subjectAllCheckbox = document.getElementById('check_all_subjects');
+
+
+    studentsAllCheckbox.onclick = handleAllCheckbox;
+    studentCheckboxes.forEach(chbx => chbx.onclick = handleStCheckbox);
+    subjectAllCheckbox.onclick = handleAllCheckbox;
+    subjectCheckboxes.forEach(chbx => chbx.onclick = handleSbjCheckbox);
+}
+
+function setAllStudentCheckbox() {
+    let studentCheckboxes = document.getElementsByName('student_id');
+    let studentsAllCheckbox = document.getElementById('check_all_students');
+
+    studentsAllCheckbox.checked = true;
+    for (let i = 0; i < studentCheckboxes.length; i++) {
+        if (!studentCheckboxes[i].checked){
+            studentsAllCheckbox.checked = false;
+            break;
+        }
+    }
+}
+
+function setAllSubjectCheckbox() {
+    let subjectCheckboxes = document.getElementsByName('subject_id');
+    let subjectAllCheckbox = document.getElementById('check_all_subjects');
+
+    subjectAllCheckbox.checked = true;
+    for (let i = 0; i < subjectCheckboxes.length; i++) {
+        if (!subjectCheckboxes[i].checked){
+            subjectAllCheckbox.checked = false;
+            break;
+        }
+    }
+}
+
+function handleAllCheckbox(event) {
+    let target = event.currentTarget;
+    if (target.id === 'check_all_students'){
+        let studentCheckboxes = document.getElementsByName('student_id');
+        let val = target.checked;
+        studentCheckboxes.forEach(chbx => chbx.checked=val);
+    } else if(target.id === 'check_all_subjects') {
+        let subjectCheckboxes = document.getElementsByName('subject_id');
+        let val = target.checked;
+        subjectCheckboxes.forEach(chbx => chbx.checked=val);
+    }
+}
+
+function handleStCheckbox(event) {
+    setAllStudentCheckbox();
+}
+
+function handleSbjCheckbox(event) {
+    setAllSubjectCheckbox();
+}
+
 function createStudentsList() {
     let studentsCheckBoxes = document.getElementsByName("student_id");
     let students = "";
